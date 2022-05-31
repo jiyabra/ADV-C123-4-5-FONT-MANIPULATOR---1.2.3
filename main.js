9ed41c7e337838ae2962edc75bee5633927ce1f1
jiyabra <86106526+jiyabra@users.noreply.github.com>
2022-05-31 04:52:07 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightwristX = 0;
+leftwristX = 0;
+Word = peter;
+
+function draw() {
+    background('Purple');
+    fill("pink");
+    stroke("grey");
+    Word(noseX, noseY, difference);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized!');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
+    }
+}
